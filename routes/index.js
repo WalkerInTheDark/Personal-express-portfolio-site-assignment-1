0c1d4deada2af0fe2cf671d953b2094cdadd5f87
WalkerInTheDark <112958474+WalkerInTheDark@users.noreply.github.com>
2022-10-13 22:11:15 +0300

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' },
+  
+  
+  );
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+router.get('/skills', function(req, res, next) {
+  res.render('Skills', { title: 'Skills' });
+});
+
+router.get('/Contact', function(req, res, next) {
+  res.render('Contact', { title: 'Contact' });
+});
+module.exports = router;
